767e6b2babfb8bd5dcd92e71ee02dec6b5885322
Oshada Maheeshan <oshadamahee@gmail.com>
2019-02-19 15:14:15 +0530

    user service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,19 @@ import { DateComponent } from './date/date.component';
 import { AddressCardComponent } from './address-card/address-card.component';
 import { ViewModule } from './view/view.module';
 import { TestService } from './test.service';
-import { HttpClientModule } from '@angular/common/http'
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     DateComponent,
-    AddressCardComponent
+    AddressCardComponent,
+    OneComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,8 @@ import { HttpClientModule } from '@angular/common/http'
     HttpClientModule
   ],
   providers: [
-    TestService
+    TestService,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
